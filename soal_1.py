20347a5e6b257bc32163b592a049b1c67723d064
Rakha404 <dawgsyogs@gmail.com>
2024-11-13 11:55:53 +0700

    Add files via upload

---
+++ soal_1.py
@@ -0,0 +1,6 @@
+nilai_a = int(input("Masukan Nilai A : "))
+nilai_b = int(input("Masukan Nilai B : "))
+
+print(f"hasil Nilai A ditambah Nilai B adalah  : {nilai_a + nilai_b}")
+print(f"hasil Nilai A dikurangi Nilai B adalah : {nilai_a - nilai_b}")
+print(f"hasil Nilai A dikali Nilai B adalah : {nilai_a * nilai_b}")
---
+++ soal_2.py
@@ -0,0 +1,5 @@
+tahun = int(input("Masukan Tahun : "))
+if tahun / 400:
+    print(f" Tahun {tahun} tersebut adalah tahun kabisat")
+else :
+    print(f" Tahun {tahun} bukan tahun kabisat ")
